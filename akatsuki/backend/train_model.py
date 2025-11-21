69d8e6bb3af4e9ebdae5b5c5ffb93b1f20b7a16d
Kishan S T <stkishan45@gmail.com>
2025-11-22 00:01:36 +0530

    Updated training script and added .gitignore

--- akatsuki/backend/train_model.py
+++ akatsuki/backend/train_model.py
@@ -18,9 +18,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Model
 model = RandomForestRegressor(
-    n_estimators=200,
+    n_estimators=40,
+    max_depth=10,
     random_state=42
 )
+
 
 # Train
 model.fit(X_train, y_train)
